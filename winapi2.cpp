eb59ffe48dd0b1682c9607f549155c8fe1e923e6
Rogerbin <viomab@sina.com>
2015-10-23 11:30:04 +0800

    显示文字，图形
    窗口重绘

---
+++ winapi2.cpp
@@ -0,0 +1,101 @@
+#include <windows.h>
+
+const char g_szClassName[] = "myWindowClass";
+
+// Step 4: the Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	//创建画刷对象
+	HBRUSH hb = CreateSolidBrush(RGB(255, 0, 0));
+	HBRUSH hb2 = CreateSolidBrush(RGB(0, 255, 0));
+
+	char * psString = "hello world";
+	switch (msg)
+	{
+	case WM_CREATE:
+		return 0;
+	case WM_PAINT:
+	
+		hdc = BeginPaint(hwnd, &ps);
+		SelectObject(ps.hdc, hb);//为当前dc选择画刷
+		SetTextColor(ps.hdc,RGB(100,22,255));
+		TextOut(hdc, 100, 100, psString, strlen(psString));
+		Rectangle(ps.hdc, 20, 40, 70, 100);
+		SelectObject(hdc, hb2);
+		Rectangle(ps.hdc, 200, 100, 80, 80);
+		
+		DeleteObject(hb2);
+		DeleteObject(hb);
+		EndPaint(hwnd, &ps);
+		
+		break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wParam, lParam);
+	}
+	return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	WNDCLASSEX wc;
+	HWND hwnd;
+	MSG Msg;
+
+	//Step 1: Registering the Window Class
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW|CS_VREDRAW;
+	wc.lpfnWndProc = WndProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW );
+	wc.lpszMenuName = NULL;
+	wc.lpszClassName = g_szClassName;
+	wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Window Registration Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	// Step 2: Creating the Window
+	hwnd = CreateWindowEx(
+		WS_EX_CLIENTEDGE,
+		g_szClassName,
+		"The title of my window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, 540, 420,
+		NULL, NULL, hInstance, NULL);
+
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window Creation Failed!", "Error!",
+			MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	// Step 3: The Message Loop
+	while (GetMessage(&Msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&Msg);
+		DispatchMessage(&Msg);
+	}
+	return Msg.wParam;
+}
